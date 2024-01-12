2b37cc06089accba832c74be0160aaa757f8f7e4
Mikey A <77662442+Mikeyar1@users.noreply.github.com>
2024-01-11 22:58:25 -0500

    Update BookResult.java

--- BookResult.java
+++ BookResult.java
@@ -1,3 +1,13 @@
+/**
+ * Class: BookResult
+ * The BookResult class represents the result of counting books in a player's hand.
+ 
+ * @author mikeyarias
+ * 
+ * - Create an instance of BookResult by providing the number of books and the rank.
+ * - Retrieve the number of books and rank using the provided getter methods.
+ */
+public 
 package CardGame;
 
 public class BookResult {
